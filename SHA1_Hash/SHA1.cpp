7bbec751e0c4ff17348a212b741debd6ff1e2bbd
alex.pershin <www.alexander17@inbox.ru>
2019-02-21 00:48:30 +0300

    Create Work Version SHA1 Hash Function:  ->SHA1.cpp, SHA1.h - compile in static library
    Testing Correct Work SHA1 Algorithm:
     ->Test_SHA1.h

--- SHA1_Hash/SHA1.cpp
+++ SHA1_Hash/SHA1.cpp
@@ -1,6 +1,10 @@
 #include "SHA1.h"
 
-void SHA1::PaddingTheMessage(){
+#define Ch(x, y, z) (((x) & (y)) ^ ((~x) & (z)))
+#define Parity(x, y, z) ((x) ^ (y) ^ (z))
+#define Maj(x, y, z) (((x) & (y)) ^ ((x) & (z)) ^ ((y) & (z)))
+
+void SHA1::PaddingTheMessage() {
 	union Transformation {
 		uint8_t BytesOfNumber[8];
 		uint64_t qwNumber;
@@ -12,17 +16,122 @@ void SHA1::PaddingTheMessage(){
 
 	byarrMessage->push_back(0x80);
 
-	uint8_t wSizeOfZeroPaddingBits = (448 - (8 * byLastBlockSize + 8)) / 8;
-	for (uint8_t i = 0; i < wSizeOfZeroPaddingBits; i++) { byarrMessage->push_back(0x00); }
+	uint8_t wSizeOfZeroPaddingBytes = (448 - (8 * byLastBlockSize + 8)) / 8;
+	for (uint8_t i = 0; i < wSizeOfZeroPaddingBytes; i++) { byarrMessage->push_back(0x00); }
 
 	transformationNumbers.qwNumber = 8 * byLastBlockSize;
 	for (int i = 7; i > -1; i--) { byarrMessage->push_back(transformationNumbers.BytesOfNumber[i]); }
-}
+};
 
-SHA1::SHA1(){
+void SHA1::Preprocessing() {
+	if (byarrMessage->size() % 64 != 0) PaddingTheMessage();
+};
 
-}
+void SHA1::HashCompulation() {
 
-SHA1::~SHA1(){
+	H0 = 0x67452301;
+	H1 = 0xefcdab89;
+	H2 = 0x98badcfe;
+	H3 = 0x10325476;
+	H4 = 0xc3d2e1f0;
 
-}
+	union FormatedMessageToAlgorithm {
+		uint32_t M;
+		uint8_t ByteArrayOfM[4];
+	};
+	FormatedMessageToAlgorithm formatMessage;
+
+	for (uint64_t i = 0; i < byarrMessage->size(); ) {
+		while (M->size() != 16) {
+			for (int j = 3; j > -1; j--) {
+				formatMessage.ByteArrayOfM[j] = (*byarrMessage)[i];
+				i++;
+			}
+			M->push_back(formatMessage.M);
+		}
+		HashComplulationBlock();
+		M->clear();
+	}
+};
+
+void SHA1::HashComplulationBlock() {
+	for (uint8_t t = 0; t < 16; t++) { W->push_back((*M)[t]); }
+	for (uint8_t t = 16; t < 80; t++) { W->push_back(ROTL(((*W)[t - 3] ^ (*W)[t - 8] ^ (*W)[t - 14] ^ (*W)[t - 16]), 1)); }
+
+	uint32_t a, b, c, d, e, f, T, K;
+	a = H0;
+	b = H1;
+	c = H2;
+	d = H3;
+	e = H4;
+
+	for (uint8_t t = 0; t < 80; t++) {
+
+		if (t < 20) {
+			f = Ch(b, c, d);
+			K = K0;
+		}
+		else if (t < 40) {
+			f = Parity(b, c, d);
+			K = K1;
+		}
+		else if (t < 60) {
+			f = Maj(b, c, d);
+			K = K2;
+		}
+		else {
+			f = Parity(b, c, d);
+			K = K3;
+		}
+
+		T = ROTL(a, 5) + f + e + K + (*W)[t];
+		e = d;
+		d = c;
+		c = ROTL(b, 30);
+		b = a;
+		a = T;
+	}
+
+	H0 += a;
+	H1 += b;
+	H2 += c;
+	H3 += d;
+	H4 += e;
+
+	W->clear();
+};
+
+uint32_t SHA1::ROTL(uint32_t x, uint8_t n) { return ((x << n) | (x >> (32 - n))); };
+
+vector<uint8_t>* SHA1::GetHash(vector<uint8_t>* ptrMessage) {
+
+	byarrMessage = ptrMessage;
+	M = make_unique<vector<uint32_t>>();
+	W = make_unique<vector<uint32_t>>();
+
+	Preprocessing();
+	HashCompulation();
+
+	union DigestFormat {
+		uint32_t dwH;
+		uint8_t byArray[4];
+	};
+	DigestFormat formatDigest;
+
+	auto Digest = new vector<uint8_t>;
+
+	formatDigest.dwH = H0;
+	for (int i = 3; i > -1; i--) { Digest->push_back(formatDigest.byArray[i]); }
+	formatDigest.dwH = H1;
+	for (int i = 3; i > -1; i--) { Digest->push_back(formatDigest.byArray[i]); }
+	formatDigest.dwH = H2;
+	for (int i = 3; i > -1; i--) { Digest->push_back(formatDigest.byArray[i]); }
+	formatDigest.dwH = H3;
+	for (int i = 3; i > -1; i--) { Digest->push_back(formatDigest.byArray[i]); }
+	formatDigest.dwH = H4;
+	for (int i = 3; i > -1; i--) { Digest->push_back(formatDigest.byArray[i]); }
+
+	H0 = H1 = H2 = H3 = H4 = 0;
+
+	return Digest;
+};
--- SHA1_Hash/SHA1.h
+++ SHA1_Hash/SHA1.h
@@ -15,212 +15,41 @@ CATEGORY: COMPUTER SECURITY SUBCATEGORY: CRYPTOGRAPHY
 #include <iostream>
 #include <cstring>
 #include <vector>
+#include <memory>
 
 using namespace std;
 
-
-#define Ch(x, y, z) (((x) & (y)) ^ ((~x) & (z)))
-#define Parity(x, y, z) ((x) ^ (y) ^ (z))
-#define Maj(x, y, z) (((x) & (y)) ^ ((x) & (z)) ^ ((y) & (z)))
-#define Parity(x, y, z) ((x) ^ (y) ^ (z))
-
-
-
 class SHA1 {
 public:
-	const uint32_t K0 = 0x5a827999;
-	const uint32_t K1 = 0x6ed9eba1;
-	const uint32_t K2 = 0x8f1bbcdc;
-	const uint32_t K3 = 0xca62c1d6;
+	const uint32_t K0{ 0x5a827999 };
+	const uint32_t K1{ 0x6ed9eba1 };
+	const uint32_t K2{ 0x8f1bbcdc };
+	const uint32_t K3{ 0xca62c1d6 };
+
+	uint32_t H0{ 0 };
+	uint32_t H1{ 0 };
+	uint32_t H2{ 0 };
+	uint32_t H3{ 0 };
+	uint32_t H4{ 0 };
 
 	vector<uint8_t>* byarrMessage{ nullptr };
 
+	unique_ptr<vector<uint32_t>> W{ nullptr };
+	unique_ptr<vector<uint32_t>> M{ nullptr };
+
 	void PaddingTheMessage();
 
+	void Preprocessing();
+
+	void HashCompulation();
+
+	void HashComplulationBlock();
+
+	uint32_t ROTL(uint32_t x, uint8_t n);
+
 public:
-	SHA1();
-	~SHA1();
+
+	vector<uint8_t>* GetHash(vector<uint8_t>* ptrMessage);
 };
-
-
-
-//class SHA12
-//{
-//private:
-//	uint32 H0, H1, H2, H3, H4;
-//	unsigned char bytes[64];
-//	int unprocessedBytes;
-//	uint32 size;
-//	void process();
-//public:
-//	SHA1();
-//	~SHA1();
-//	void addBytes(const char* data, int num);
-//	unsigned char* getDigest();
-//	static uint32 ROTL(uint32 x, int bits);
-//	static void uint32_to_uchar(unsigned char* byte, uint32 num);
-//};
-//
-//
-//
-//uint32 SHA1::ROTL(uint32 x, int bits)
-//{
-//	return (x << bits) | (x >> (32 - bits));
-//};
-//
-//
-//void SHA1::uint32_to_uchar(unsigned char* byte, uint32 num)
-//{
-//	assert(byte);
-//	byte[0] = (unsigned char)(num >> 24);
-//	byte[1] = (unsigned char)(num >> 16);
-//	byte[2] = (unsigned char)(num >> 8);
-//	byte[3] = (unsigned char)num;
-//}
-//
-//
-//// Constructor *******************************************************
-//SHA1::SHA1()
-//{
-//	// проверка на корректность типа uint32
-//	assert(sizeof(uint32) * 5 == 20);
-//
-//	// начальные данные
-//	H0 = 0x67452301;
-//	H1 = 0xefcdab89;
-//	H2 = 0x98badcfe;
-//	H3 = 0x10325476;
-//	H4 = 0xc3d2e1f0;
-//	unprocessedBytes = 0;
-//	size = 0;
-//}
-//
-//// Destructor ********************************************************
-//SHA1::~SHA1()
-//{
-//	// данные на выходе
-//	H0 = H1 = H2 = H3 = H4 = 0;
-//	for (int c = 0; c < 64; c++) bytes[c] = 0;
-//	unprocessedBytes = size = 0;
-//}
-//
-//// process ***********************************************************
-//void SHA1::process()
-//{
-//	assert(unprocessedBytes == 64);
-//
-//	int t;
-//	uint32 a, b, c, d, e, K, f, W[80];
-//
-//	a = H0;
-//	b = H1;
-//	c = H2;
-//	d = H3;
-//	e = H4;
-//	/*–асширение*/
-//	for (t = 0; t < 16; t++) { W[t] = (bytes[t * 4] << 24) + (bytes[t * 4 + 1] << 16) + (bytes[t * 4 + 2] << 8) + bytes[t * 4 + 3]; }
-//	for (; t < 80; t++) { W[t] = ROTL(W[t - 3] ^ W[t - 8] ^ W[t - 14] ^ W[t - 16], 1); }
-//
-//	/*—жатие */
-//	uint32 temp;
-//	for (t = 0; t < 80; t++)
-//	{
-//		if (t < 20) {
-//			K = 0x5a827999;
-//			f = (b & c) ^ ((~b) & d);
-//		}
-//		else if (t < 40) {
-//			K = 0x6ed9eba1;
-//			f = b ^ c ^ d;
-//		}
-//		else if (t < 60) {
-//			K = 0x8f1bbcdc;
-//			f = (b & c) ^ (b & d) ^ (c & d);
-//		}
-//		else {
-//			K = 0xca62c1d6;
-//			f = b ^ c ^ d;
-//		}
-//		temp = ROTL(a, 5) + f + e + W[t] + K;
-//		e = d;
-//		d = c;
-//		c = ROTL(b, 30);
-//		b = a;
-//		a = temp;
-//	}
-//	/* —уммирование */
-//	H0 += a;
-//	H1 += b;
-//	H2 += c;
-//	H3 += d;
-//	H4 += e;
-//
-//	unprocessedBytes = 0;
-//}
-//
-//// addBytes **********************************************************
-//void SHA1::addBytes(const char* data, int num)
-//{
-//	assert(data);
-//	assert(num > 0);
-//
-//	size += num;
-//
-//	while (num > 0)
-//	{
-//
-//		int needed = 64 - unprocessedBytes;
-//		assert(needed > 0);
-//		// число байт дл€ копировани€ в 512 битовый блок
-//		int toCopy = (num < needed) ? num : needed;
-//		// копирование байт
-//		memcpy(bytes + unprocessedBytes, data, toCopy);
-//
-//		num -= toCopy;
-//		data += toCopy;
-//		unprocessedBytes += toCopy;
-//
-//		// проверка на полноту блока (512бит==64байта)
-//		if (unprocessedBytes == 64) process();
-//	}
-//}
-//
-//// digest ************************************************************
-//unsigned char* SHA1::getDigest()
-//{
-//	// ’ранение длины сообщени€
-//	uint32 totalBitsL = size << 3;
-//	uint32 totalBitsH = size >> 29;
-//	// добавление 0x80 в последний некратный блок
-//	addBytes("\x80", 1);
-//
-//	//массив, в котором хранитс€ последний-дополненный блок
-//	unsigned char footer[64] = {
-//		0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-//		0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-//		0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-//		0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
-//
-//	if (unprocessedBytes > 56)
-//		addBytes((char*)footer, 64 - unprocessedBytes);
-//	assert(unprocessedBytes <= 56);
-//	// сколько нулей нужно добавить после '1'
-//	int neededZeros = 56 - unprocessedBytes;
-//	// хранение размера сообщени€ max(64 бит) в конце массива footer(512)
-//	uint32_to_uchar(footer + neededZeros, totalBitsH);
-//	uint32_to_uchar(footer + neededZeros + 4, totalBitsL);
-//	// окончание формировани€ последнего (дополненного) блока
-//	addBytes((char*)footer, neededZeros + 8);
-//	// хранение дайджаста (хэш-значени€) сообщени€
-//	unsigned char* digest = new unsigned char[20];
-//	// копирование дайджаста из unsigned int -> unsigned char
-//	uint32_to_uchar(digest, H0);
-//	uint32_to_uchar(digest + 4, H1);
-//	uint32_to_uchar(digest + 8, H2);
-//	uint32_to_uchar(digest + 12, H3);
-//	uint32_to_uchar(digest + 16, H4);
-//	// return digest
-//	return digest;
-//}
 
 #endif //SHA1_H
